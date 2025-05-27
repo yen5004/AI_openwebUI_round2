5fe76b3cf51924854f2d7199f7590ea997284e7c
yen5004 <119946331+yen5004@users.noreply.github.com>
2025-05-27 18:00:44 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# Check docker and install docker
 docker --version
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
@@ -7,3 +8,15 @@ sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 docker-compose --version
+
+
+# install ollama
+curl -fsSL https://ollama.com/install.sh | sh
+ollama --version
+ollama pull gemma3:1b
+ollama serve &
+sudo systemctl start docker
+docker-compose up -d
+
+# check webpage for activity
+echo " Visit http://localhost:8080"
